d0e382e7f80d1fe2528c376b8c6e260c5ba78225
kmagai <koki.kumagai@dena.com>
2016-07-28 00:55:27 +0900

    fix

--- problem_4/largest_palindrome_product.go
+++ problem_4/largest_palindrome_product.go
@@ -6,10 +6,21 @@ import "fmt"
 // Find the largest palindrome made from the product of two 3-digit numbers.
 
 func main() {
+	max := 0
 	for i := 999; i > 0; i-- {
 		for r := 999; r > 0; r-- {
-			n := i * r
-			fmt.Println(n)
+			if n := i * r; n == reverse(n) && n > max {
+				max = n
+			}
 		}
 	}
+	fmt.Println(max)
+}
+
+func reverse(n int) int {
+	acm := 0
+	for ; n > 1; n = n / 10 {
+		acm = acm*10 + n%10
+	}
+	return acm
 }
